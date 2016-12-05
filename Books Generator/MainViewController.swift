999d42773bb858c364e42647a39925cbc43e1523
Ali Soliman <ali.soliman95@gmail.com>
2016-12-05 02:31:06 +0200

    Remove unused method

--- Books Generator/MainViewController.swift
+++ Books Generator/MainViewController.swift
@@ -13,13 +13,7 @@ class MainViewController: UIViewController {
     
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        Alamofire.request(Constants.BASE_URL + Constants.WELCOME_URL).responseJSON { response in
-            if let JSON = response.result.value {
-                print("JSON: \(JSON)")
-            }
-        }
-        
+        super.viewDidLoad()        
     }
 
 }
